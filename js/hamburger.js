55a202c77585bb5755e557677a74994af4059f0e
Max <Floota591@gmail.com>
2024-12-12 13:03:52 +0100

    ADD: Menu buttons scrolls to section

--- js/hamburger.js
+++ js/hamburger.js
@@ -3,9 +3,35 @@
 let burgerButton = document.getElementsByClassName('navbar__menu')[0]
 let burgerMenu = document.getElementsByClassName('navbar__list')[0]    
 let whyItem = document.getElementById('why')
+let benefitItem = document.getElementById('benefit')
+let pricesItem = document.getElementById('prices')
+let contactItem = document.getElementById('contact')
+
 
 burgerButton.addEventListener("click", () => {
 toggleElement(burgerMenu)
 burgerButton.innerHTML = burgerButton.innerHTML === '☰' ? '✕' : '☰'
+if (window.matchMedia("(min-width: 700px)").matches) {
+ console.log("damn")   
+}
 })
 
+whyItem.addEventListener('click', () => window.scrollTo({
+    top: 850,
+    behavior: 'smooth',
+  }));
+
+benefitItem.addEventListener('click', () => window.scrollTo({
+    top: 1200,
+    behavior: 'smooth',
+  }));
+
+  pricesItem.addEventListener('click', () => window.scrollTo({
+    top: 2300,
+    behavior: 'smooth',
+  }));
+
+  contactItem.addEventListener('click', () => window.scrollTo({
+    top: document.body.scrollHeight,
+    behavior: 'smooth',
+  }));
